d7e03571684da353a30607584d7a7847c105c7b1
Florian Reifschneider <flore2003@googlemail.com>
2016-11-19 23:08:02 +0100

    Additional info page

--- src/app/components/additional-info-page/additional-info-page.component.ts
+++ src/app/components/additional-info-page/additional-info-page.component.ts
@@ -6,22 +6,28 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './additional-info-page.component.html',
 })
 export class AdditionalInfoPageComponent implements OnInit {
-	deductible: string[] = ['0 €', '150 €', '250 €', '500 €', '1000 €'];
+	deductible: string[] = ['0€', '150€', '250€', '500€', '1000€'];
 	deductibleCodes: string[] = ['tc_no_deductible', 'tc_150_eur', 'tc_250_eur', 'tc_500_eur', 'tc_1000_eur'];
 
     coverageType: string[] = ['Bronze', 'Silver', 'Gold'];
     coverageTypeCodes: string[] = ['tc_bronze', 'tc_silver', 'tc_gold'];
 
 
-    private selectedDeductible_HA: number = 1;
-    private selectedDeductible_RS: number = 2;
+    selectedDeductible_HA: number = 1;
+    selectedDeductible_RS: number = 2;
 
-    private selectedCoverageType_HA: number = 1;
-    private selectedCoverageType_RS: number = 1;
+    selectedCoverageType_HA: number = 1;
+    selectedCoverageType_RS: number = 1;
+
+    familyStatus: string;
 
     constructor() { }
 
     ngOnInit() {
+    }
+
+    onSubmit() {
+        console.log("Test");
     }
 
 }
